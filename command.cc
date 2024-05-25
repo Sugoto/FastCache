3bba90c49cee8b75e2e3940d5dc38bb4127fc914
Sugoto <sugotobasu1@gmail.com>
2024-05-25 18:49:33 +0530

    Refactor Command and EpollManager classes

--- command.cc
+++ command.cc
@@ -1,28 +1,35 @@
 #include "command.h"
 
 Command::Command()
-: Command(0) {
+    : Command(0)
+{
 }
 
 Command::Command(size_t args_size)
-: args_size{args_size} {
+    : args_size{args_size}
+{
 }
 
-bool Command::addArgs(std::string&& s) {
-  if (isEmpty() || isComplete()) return false;
+bool Command::addArgs(std::string &&s)
+{
+  if (isEmpty() || isComplete())
+    return false;
   args.push_back(std::move(s));
   return true;
 }
 
-void Command::reset() {
+void Command::reset()
+{
   args_size = 0;
   args.clear();
 }
 
-bool Command::isEmpty() {
+bool Command::isEmpty()
+{
   return args_size == 0;
 }
 
-bool Command::isComplete() {
+bool Command::isComplete()
+{
   return !isEmpty() && args.size() == args_size;
 }
